3995d02a74e1bde501bce707b05fa190ab7e738d
Wei TingTao <s952865@go.thu.edu.tw>
2015-11-03 22:09:55 -0600

    Initial Commit

---
+++ VoiceAM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VoiceAM
+//
+//  Created by Wei TingTao on 11/3/15.
+//  Copyright © 2015 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
